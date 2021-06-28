30187a3adc9ed7e680b33dfbe024bcc644adeb1d
Dmitriy <43322960+ZaycevDmitriy@users.noreply.github.com>
2021-06-29 00:06:57 +0300

    Выполнил задание урока 18

--- script/app.js
+++ script/app.js
@@ -1,6 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
+  // Таймер
+
   const countTimer = (deadline) => {
     const timerHours = document.getElementById('timer-hours'),
       timerMinutes = document.getElementById('timer-minutes'),
@@ -41,4 +43,73 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   countTimer('18:40 27 June 2021');
+
+  // Меню
+
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+      menu = document.querySelector('menu'),
+      closeBtn = document.querySelector('.close-btn'),
+      menuItems = menu.querySelectorAll('ul>li');
+
+    const handlerMenu = () => {
+      // if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+      //   menu.style.transform = `translate(0)`;
+      // } else {
+      //   menu.style.transform = `translate(-100%)`;
+      // }
+
+      menu.classList.toggle('active-menu');
+    };
+
+    btnMenu.addEventListener('click', handlerMenu);
+
+    closeBtn.addEventListener('click', handlerMenu);
+
+    menuItems.forEach((item) => item.addEventListener('click', handlerMenu));
+  };
+
+  toggleMenu();
+
+  // popup
+
+  const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+      popupBtn = document.querySelectorAll('.popup-btn'),
+      popupClose = document.querySelector('.popup-close'),
+      popupContent = document.querySelector('.popup-content');
+    let popupInterval;
+    let counter = -50;
+
+    const animationPopUp = () => {
+      popupInterval = requestAnimationFrame(animationPopUp);
+
+      counter += 1;
+      if (counter < 39) {
+        popupContent.style.left = `${counter}%`;
+      } else {
+        cancelAnimationFrame(popupInterval);
+        counter = -50;
+      }
+    };
+
+    popupBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+        let width = document.documentElement.clientWidth;
+        if (width < 768) {
+          popup.style.display = 'block';
+        } else {
+          popupContent.style.left = '-50%';
+          popup.style.display = 'block';
+          popupInterval = requestAnimationFrame(animationPopUp);
+        }
+      });
+    });
+
+    popupClose.addEventListener('click', () => {
+      popup.style.display = 'none';
+    });
+  };
+
+  togglePopUp();
 });
