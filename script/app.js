afe48d1e2852027c056bba4050a84ed82b8bee7a
dmitrii_zaitsev <zaycevdi@gmail.com>
2021-07-03 14:41:20 +0300

    Закрытие меню при клике в не окна меню.

--- script/app.js
+++ script/app.js
@@ -49,19 +49,27 @@ window.addEventListener('DOMContentLoaded', () => {
   const toggleMenu = () => {
     const btnMenu = document.querySelector('.menu'),
       menu = document.querySelector('menu');
+    
+    const closeMenu = (event) => {
+      const target = event.target;
+      const targetCloseLi = target.closest('menu>ul>li');
+      console.log(target.closest('menu'));
+
+      if (targetCloseLi || target.classList.contains('close-btn')) handlerMenu();
+      if(!target.closest('menu') && !target.closest('.menu')) handlerMenu();
+    }
 
     const handlerMenu = () => {
       menu.classList.toggle('active-menu');
+      if (menu.classList.contains('active-menu')) {
+        document.body.addEventListener('click', closeMenu);
+      } else {
+        document.body.removeEventListener('click', closeMenu);
+      }
     };
 
     btnMenu.addEventListener('click', handlerMenu);
 
-    menu.addEventListener('click', (event) => {
-      const target = event.target;
-      const targetCloseLi = target.closest('menu>ul>li');
-
-      if (targetCloseLi || target.classList.contains('close-btn')) handlerMenu();
-    });
   };
 
   toggleMenu();
