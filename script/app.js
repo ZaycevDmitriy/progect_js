1b1a8ed124f88d7607f1f9d27edd21164adbaaf5
dmitrii_zaitsev <zaycevdi@gmail.com>
2021-07-12 22:42:02 +0300

    Добавил переключение картинок при наведение.

--- script/app.js
+++ script/app.js
@@ -260,6 +260,25 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   slider();
+
+  //change images on hover
+
+  const changeImage = () => {
+    const commandPhotos = document.querySelectorAll('.command__photo');    
+    commandPhotos.forEach((photo) => {
+      let tempSrcPhoto;
+      photo.addEventListener('mouseenter', () => {
+        tempSrcPhoto = photo.src;
+        photo.src = photo.dataset.img;
+      });
+
+      photo.addEventListener('mouseleave', () => {
+        photo.src = tempSrcPhoto;
+      });
+    });
+  };
+
+  changeImage();
   
     // validation
 
