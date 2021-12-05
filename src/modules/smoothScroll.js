3f4b673e7e30d089f16fb7f6d3921ba5efbbd9e7
dmitrii_zaitsev <zaycevdi@gmail.com>
2021-12-05 15:23:23 +0300

    Исправил скролл

--- src/modules/smoothScroll.js
+++ src/modules/smoothScroll.js
@@ -1,35 +1,45 @@
 'use strict';
 
 const smoothScroll = () => {
-  const scrollBtn = document.querySelector('[href^="#service-block"]');
-  const SPEED = 1;
-
+  const SPEED = 0.5;
   const scrolled = e => {
     e.preventDefault();
 
-    let start = 0;
-    const pageY = window.pageYOffset;
+    const target = e.target;
 
-    const coordinateElem = scrollBtn.getBoundingClientRect().top;
+    if (target.matches('[href^="#"]')) {
+      let start = 0;
 
-    const step = time => {
-      if (!start) start = time;
-      const progress = time - start;
+      const pageY = window.pageYOffset;
 
-      const r = (coordinateElem < 0 ?
-        Math.max(pageY - progress / SPEED, pageY + coordinateElem) :
-        Math.min(pageY + progress / SPEED, pageY + coordinateElem));
+      const hash = target.getAttribute('href');
 
-      window.scrollTo(0, r);
+      if (hash === '#') return
 
-      if (r < pageY + coordinateElem) requestAnimationFrame(step);
+      const elem = document.querySelector(hash);
 
-    };
+      const coordinateElem = elem.getBoundingClientRect().top;
 
-    requestAnimationFrame(step);
+      const step = time => {
+        if (!start) start = time;
+        const progress = time - start;
+
+        const r = (coordinateElem < 0 ?
+          Math.max(pageY - progress / SPEED, pageY + coordinateElem) :
+          Math.min(pageY + progress / SPEED, pageY + coordinateElem))
+
+        window.scrollTo(0, r);
+
+        if (r < pageY + coordinateElem) requestAnimationFrame(step);
+
+      }
+
+      requestAnimationFrame(step)
+    }
   };
 
-  scrollBtn.addEventListener('click', scrolled);
+  document.body.addEventListener('click', scrolled);
 }
 
 export default smoothScroll;
+
